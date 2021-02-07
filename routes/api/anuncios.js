30940ffff9d6208c4ea699b44b7634675daa0fb3
Andrés <usuario616@gmail.com>
2021-02-07 15:35:18 +0100

    Separar módulo de filtrado

--- routes/api/anuncios.js
+++ routes/api/anuncios.js
@@ -3,61 +3,15 @@ var router = express.Router();
 
 const Anuncio = require('../../models/Anuncio');
 
+
+
 /* GET /api/Anuncios */
 // Lista de Anuncios
 router.get('/', async function(req, res, next) {
 
   try {
-
-    const nombre = req.query.nombre;
-    let precio = req.query.precio;
-    const id = req.query.id;
-    const venta = req.query.venta;
-    const tag = req.query.tag;
-    const limit = parseInt(req.query.limit);
-    const skip = parseInt(req.query.start);
-    const fields = req.query.fields;
-    const sort = req.query.sort;
-
-    const filtro = {};
-
-    if (nombre) {
-      filtro.nombre = { $regex: `^${nombre}`, $options: 'i' };
-    }
-
-    if (id) {
-      filtro._id = id;
-    }
-
-    if (venta) {
-      if(venta==='true' || venta==='false'){
-        filtro.venta = venta==='true';
-      }
-    }
-
-    if (tag) {
-      filtro.tags = { $all: tag } ;
-    }
-
-    const regexJusto = new RegExp('^[0-9]*$');
-    const regexRango = new RegExp('^[0-9]*-[0-9]*$');
-    const regexMin = new RegExp('^[0-9]*-$');
-    const regexMax = new RegExp('^-[0-9]*$');
-    if(precio){
-      if (precio.match(regexJusto)) {
-        filtro.precio = precio;
-      }else if(precio.match(regexMin)){
-        precio = precio.replace('-','');
-        filtro.precio = { $gte: precio };
-      }else if(precio.match(regexMax)){
-        precio = precio.replace('-','');
-        filtro.precio = { $lte: precio};
-      }else if(precio.match(regexRango)) {
-        const rango = precio.split('-');
-        if( parseInt(rango[0]) > parseInt(rango[1]) )  next(Error('El mínimo debe ser mayor que el máximo'));
-        filtro.precio = { $gte: rango[0], $lte: rango[1]};
-      }
-    }
+    const filtering = require('../functions');
+    const {filtro, limit, skip, fields, sort} = filtering(req);
     const resultado = await Anuncio.lista(filtro, limit, skip, fields, sort);
     res.json(resultado);
   } catch (err) {
