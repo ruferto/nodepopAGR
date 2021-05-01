faccfa3e9870fd8c0b2490170046489d8c08fdf3
Andrés <usuario616@gmail.com>
2021-05-02 00:12:14 +0200

    Fixed urls in resize.js

--- microservices/resize.js
+++ microservices/resize.js
@@ -4,13 +4,13 @@ const jimp = require('jimp');
 async function resize(imgPath, imgName) {
   // Read the image.
   try {
-    const image = await jimp.read('../' + imgPath + imgName);
+    const image = await jimp.read(imgPath + imgName);
 
     // Resize the image to height 150 and auto width.
     await image.resize(jimp.AUTO, 100);
 
     // Save and overwrite the image
-    await image.writeAsync('../' + imgPath + '/thumbs/' + imgName);
+    await image.writeAsync(imgPath + '/thumbs/' + imgName);
   } catch (error) {
     console.error(error);
   }
